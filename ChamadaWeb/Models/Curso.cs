9056693443860f05f38b0294da1080cf311eb932
univem <univem@univem.edu.br>
2017-05-15 22:14:49 -0300

    Inicio

---
+++ ChamadaWeb/Models/Curso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChamadaWeb.Models
+{
+    public class Curso
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ ChamadaWeb/Models/Professor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChamadaWeb.Models
+{
+    public class Professor
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public ICollection<Curso> Cursos { get; set; }
+    }
+}
